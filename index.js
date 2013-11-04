96cfe940f3d2f26b82ba3160a66bdcdc244ee5e9
Damon Oehlman <damon.oehlman@gmail.com>
2013-11-05 09:23:24 +1100

    Removed separate match file (not worth the hassle)

--- index.js
+++ index.js
@@ -27,8 +27,41 @@ var templates = [];
   the templates that have been registered previously.
 
 **/
-var unistroke = module.exports = require('./match');
+var unistroke = module.exports = function(input, opts) {
+  var best = Infinity;
+  var selected = -1;
+  var d;
+  var result;
 
+  // if we have an array as input, convert to a stroke
+  if (Array.isArray(input)) {
+    input = new Stroke(input, opts);
+  }
+
+  // if we don't have a stroke, then complain
+  if (! (input instanceof Stroke)) {
+    return;
+  }
+
+  // compare the with the registered templates
+  for (var ii = templates.length; input && ii--; ) {
+    var diff = optimalCosineDistance(templates[ii].vector, input.vector);
+
+    // if this is a better match then the current match,
+    // then update the best template index
+    if (diff < best) {
+      best = diff;
+      selected = ii;
+    }
+  }
+
+  // if we found a match, return a cloned instance of the stroke
+  if (selected >= 0) {
+    result = templates[selected].createResult(1 / best);
+  }
+
+  return result;
+};
 
 /**
   ### unistroke.define(template)
@@ -56,3 +89,21 @@ define(require('./templates/lsb'));
 define(require('./templates/rsb'));
 define(require('./templates/triangle'));
 define(require('./templates/x'));
+
+/* internal helper functions */
+
+function optimalCosineDistance(v1, v2) {
+  var a = 0;
+  var b = 0;
+  var angle;
+  var ii = 0;
+  var count = v1.length;
+
+  for (ii = 0; ii < count; ii += 2) {
+    a += v1[ii] * v2[ii] + v1[ii + 1] * v2[ii + 1];
+    b += v1[ii] * v2[ii + 1] - v1[ii + 1] * v2[ii];
+  }
+
+  angle = Math.atan(b / a);
+  return b === 0 ? b : Math.acos(a * Math.cos(angle) + b * Math.sin(angle));
+}
