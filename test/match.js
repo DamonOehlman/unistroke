3000c6d95945a7e32aea2a1ff2f8b0863c761644
Damon Oehlman <damon.oehlman@gmail.com>
2013-11-05 09:04:21 +1100

    Completed match tests

--- test/match.js
+++ test/match.js
@@ -10,3 +10,33 @@ test('left-square-bracket', function(t) {
   t.ok(match, 'got match');
   t.equal(match.name, 'left-square-bracket', 'is a left-square-bracket');
 });
+
+test('right-square-bracket', function(t) {
+  var match;
+
+  t.plan(2);
+  match = stroke(require('../templates/rsb').points);
+
+  t.ok(match, 'got match');
+  t.equal(match.name, 'right-square-bracket', 'is a right-square-bracket');
+});
+
+test('triangle', function(t) {
+  var match;
+
+  t.plan(2);
+  match = stroke(require('../templates/triangle').points);
+
+  t.ok(match, 'got match');
+  t.equal(match.name, 'triangle', 'is a triangle');
+});
+
+test('x', function(t) {
+  var match;
+
+  t.plan(2);
+  match = stroke(require('../templates/x').points);
+
+  t.ok(match, 'got match');
+  t.equal(match.name, 'x', 'is x');
+});
