3676e194737447cb2864f2c8d8979df89d581419
Damon Oehlman <damon.oehlman@gmail.com>
2013-11-04 21:48:44 +1100

    Removed mocha browser shim

--- test/match.js
+++ test/match.js
@@ -10,22 +10,3 @@ test('left-square-bracket', function(t) {
   t.ok(match, 'got match');
   t.equal(match.name, 'left-square-bracket', 'is a left-square-bracket');
 });
-
-// var unistroke = require('../unistroke'),
-//     expect = require('expect.js'),
-//     strokes = [
-//         'left-square-bracket', 
-//         'right-square-bracket',
-//         'triangle',
-//         'x'
-//     ];
-
-// describe('exact match tests', function() {
-//     strokes.forEach(function(stroke) {
-//         it('should be able to recognize an ' + stroke, function() {
-//             var match = unistroke.match(unistroke.get(stroke));
-            
-//             expect(match.stroke.name).to.equal(stroke);
-//         });
-//     });
-// });
